11941e2cecc316dcda4398efeed709f2b09ca6cb
Sejgit28 <108257892+Sejgit28@users.noreply.github.com>
2022-09-08 10:50:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className="App">
+      <h1>WELCOME TO JUMANJI</h1>
+       <h3> Everthing is possible with us</h3>
+       <h6> Assignment 1 studied and implemented successfully</h6>
+      
+    </div>
+  )
+}
+
+export default App;
